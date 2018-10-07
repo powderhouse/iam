801557a0cef3173fc8dd14460077923874b12a17
Jared Cosulich <jared.cosulich@gmail.com>
2018-10-07 10:24:27 -0400

    experimenting

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
       .library(name: "SwiftAWSIam", targets: ["SwiftAWSIam"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0"))
+      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", from: "2.0.0")
   ],
   targets: [
       .target(name: "SwiftAWSIam", dependencies: ["AWSSDKSwiftCore"]),
